84cd493bda00d764780ce4173e5f6a62fb564b10
tsucchy27 <tsucchi0627@gmail.com>
2023-05-14 00:39:44 +0900

    rushイベント追加

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 import discord
 import random
 import re
+import time
+import asyncio
 import dauth
 
 client = discord.Client(intents=discord.Intents.all())
@@ -8,6 +10,26 @@ fastHunt = "!"
 num = 0     # RUSHまでの回数
 score = 0   # スコア
 bet = 4     # 1玉何円か
+
+# ラッシュ突入時
+async def rush(message):
+    global num, score, bet
+
+    print("rush event")
+    await message.channel.send("RUSH突入!!40以下でRUSH継続!!")
+    f = True
+    while(f):
+        # ランダムな値を出力
+        r = random.randint(1, 100)
+        time.sleep(1)
+        await message.channel.send(r)
+        # 判定
+        time.sleep(1)
+        if r <= 40:
+            await message.channel.send("RUSH継続!!")
+        else:
+            f = False
+            await message.channel.send("RUSH終了")
 
 @client.event
 async def on_ready():
@@ -32,7 +54,7 @@ async def on_message(message):
         num += 1
 
         # botの手
-        b = random.randint(1, 10)
+        b = random.randint(1, 319)
         print(f"player:{p} bot:{b}")
         await message.channel.send(f"{b} ({num})")
 
@@ -41,5 +63,6 @@ async def on_message(message):
             print(f"{num}回目で当たり")
             num = 0
             await message.channel.send("当たり")
+            await asyncio.create_task(rush(message))
 
 client.run(dauth.PACHINKO_TOKEN)
