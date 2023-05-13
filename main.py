9755b92c85f232030643b6db06182abfbbe318b5
tsucchy27 <tsucchi0627@gmail.com>
2023-05-14 02:02:36 +0900

    rushイベント修正・回数追加

--- main.py
+++ main.py
@@ -11,25 +11,44 @@ num = 0     # RUSHまでの回数
 score = 0   # スコア
 bet = 4     # 1玉何円か
 
-# ラッシュ突入時
+# ラッシュチャンス時
 async def rush(message):
     global num, score, bet
-
+    rush_num = 0    # RUSH回数
     print("rush event")
-    await message.channel.send("RUSH突入!!40以下でRUSH継続!!")
+
+    # 突入判定
+    time.sleep(0.2)
+    await message.channel.send("RUSHチャンス!!60以下でRUSH突入!!")
+    r = random.randint(1, 100)
+
+    # 値出力
+    time.sleep(1)
+    await message.channel.send(r)
+    time.sleep(0.2)
+    if r > 60:
+        await message.channel.send("RUSH失敗...")
+        return
+
+    # RUSH突入
+    await message.channel.send("RUST突入!!80以下でRUSH継続!!")
+    print(f"{r} - RUSH突入")
     f = True
     while(f):
-        # ランダムな値を出力
+        # 値出力
         r = random.randint(1, 100)
         time.sleep(1)
         await message.channel.send(r)
-        # 判定
-        time.sleep(1)
-        if r <= 40:
-            await message.channel.send("RUSH継続!!")
+        rush_num += 1
+        
+        # 継続判定
+        time.sleep(0.2)
+        if r <= 80:
+            await message.channel.send(f"RUSH継続!! [{rush_num}]")
         else:
             f = False
-            await message.channel.send("RUSH終了")
+            await message.channel.send(f"RUSH終了 [{rush_num}]")
+            print(f"{r} - RUSH終了")
 
 @client.event
 async def on_ready():
@@ -54,7 +73,7 @@ async def on_message(message):
         num += 1
 
         # botの手
-        b = random.randint(1, 319)
+        b = random.randint(1, 5)
         print(f"player:{p} bot:{b}")
         await message.channel.send(f"{b} ({num})")
 
